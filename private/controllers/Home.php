6f020d5c1c7693e0849e8d0b16cc078f72e2493d
FahadulHoq <fahadulhoq@gmail.com>
2025-01-04 18:57:19 +0600

    login logout auth

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -8,31 +8,11 @@ class Home extends Controller
 	
 	function index()
 	{
-		// code...
-		// $user = $this->load_model('User');
-        $user = new User();
-        
-		//insert
-		// $arr = [
-        //     'firstname' => 'boom',
-        //     'lastname' => 'Banda',
-        //     'email' => 'eathorne@yahoo.com',
-        //     'date' => '2021-08-10 19:08:58',
-        //     'user_id' => 'eathorne.banda',
-        //     'gender' => 'male',
-        //     'school_id' => "0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH",
-        //     'rank' => 'super_admin',
-        //     'password' => password_hash('$2y$10$DfpqgNj.g4qKLJCVs9CC5esat5K0jMF49cx6wt4h0B8ZBzw6Ocrci', PASSWORD_BCRYPT),
-        //     'image' => 'uploads/cardinal_1585485603.jpg',
-        // ];
-		// $user->insert($arr);
+	    if (!Auth::logged_in()) {
+			$this->redirect('login');
+		}
 
-		//update
-		// $arr['firstname'] = 'aaa';
-		// $user->update(3,$arr);
-
-		//delete
-		// $user->delete(3);
+		$user = new User;
 
 		$data = $user->findAll();
 		$this->view('home',['rows'=>$data]);
