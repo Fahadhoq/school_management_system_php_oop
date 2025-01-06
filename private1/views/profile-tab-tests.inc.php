39aa805e8b7d34fbd793860bee7aaa1a762546b2
FahadulHoq <fahadulhoq@gmail.com>
2025-01-06 12:27:12 +0600

    permission

--- private1/views/profile-tab-tests.inc.php
+++ private1/views/profile-tab-tests.inc.php
@@ -1,4 +1,4 @@
-<center><h4>My Classes</h4></center>
+<h3>Tests</h3>
 <nav class="navbar navbar-light bg-light">
   <form class="form-inline">
     <div class="input-group">
@@ -9,8 +9,3 @@
     </div>
   </form>
 </nav>
- 
-<hr>
-<?php $rows = $student_classes;?>
-<?php include(views_path('classes'))?>
-
