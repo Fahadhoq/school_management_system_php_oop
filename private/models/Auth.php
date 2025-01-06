57cc70465edb2d67ff3f1902705209cd25129626
FahadulHoq <fahadulhoq@gmail.com>
2025-01-06 11:22:41 +0600

    lecturer

--- private/models/Auth.php
+++ private/models/Auth.php
@@ -56,7 +56,7 @@ class Auth
 
 	public static function switch_school($id)
 	{
-		if(isset($_SESSION['USER']) && ($_SESSION['USER']->rank == 'super_admin' || $_SESSION['USER']->rank == 'admin') )
+		if(isset($_SESSION['USER']) && $_SESSION['USER']->rank == 'super_admin')
 		{
 			$user = new User();
 			$school = new School();
